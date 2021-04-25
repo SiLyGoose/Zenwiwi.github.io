fdb00542b896ca0b92a16afde3ec9ab7ade23236
Simon Ly <zzisandstormzz9@gmail.com>
2021-04-24 19:58:01 -0700

    refactor site

--- js/main.js
+++ js/main.js
@@ -1,43 +1,43 @@
-jQuery(document).ready(function($) {
-  // Remove url text on mouseover for icons links
-  $('.icon a').each(function() {
-    $(this).attr('onclick', 'window.location.href="' + $(this).attr('href') + '"');
-    $(this).removeAttr('href');
-  });
-});
+// jQuery(document).ready(function($) {
+//   // Remove url text on mouseover for icons links
+//   $('.icon a').each(function() {
+//     $(this).attr('onclick', 'window.location.href="' + $(this).attr('href') + '"');
+//     $(this).removeAttr('href');
+//   });
+// });
 
 
-jQuery(document).ready(function($) {
-  /**
-   * Set footer always on the bottom of the page
-   */
-  function footerAlwayInBottom(footerSelector) {
-    var docHeight = $(window).height();
-    var footerTop = footerSelector.position().top + footerSelector.height();
-    if (footerTop < docHeight) {
-      footerSelector.css("margin-top", (docHeight - footerTop) + "px");
-    } else {
-      footerSelector.css('margin-top', '0px');
+jQuery(document).ready(function ($) {
+    /**
+     * Set footer always on the bottom of the page
+     */
+    function footerAlwayInBottom(footerSelector) {
+        var docHeight = $(window).height();
+        var footerTop = footerSelector.position().top + footerSelector.height();
+        if (footerTop < docHeight) {
+            footerSelector.css("margin-top", (docHeight - footerTop) + "px");
+        } else {
+            footerSelector.css('margin-top', '0px');
+        }
     }
-  }
-  // Apply when page is loading
-  footerAlwayInBottom($("#footer"));
-
-  // Apply when page is fully loaded
-  $(window).on("load", function() {
+    // Apply when page is loading
     footerAlwayInBottom($("#footer"));
-    $(window).trigger('resize');
-  });
 
-  // Apply when page is resizing
-  $(window).resize(function() {
-    footerAlwayInBottom($("#footer"));
-  });
+    // Apply when page is fully loaded
+    $(window).on("load", function () {
+        footerAlwayInBottom($("#footer"));
+        $(window).trigger('resize');
+    });
 
-  // Apply every 25 ms
-  window.setInterval(function() {
-    footerAlwayInBottom($("#footer"));
-  }, 25);
+    // Apply when page is resizing
+    $(window).resize(function () {
+        footerAlwayInBottom($("#footer"));
+    });
+
+    // Apply every 25 ms
+    window.setInterval(function () {
+        footerAlwayInBottom($("#footer"));
+    }, 25);
 
 });
 
@@ -47,7 +47,7 @@ setInterval(() => {
         minute = now.minutes() * 6 + second / 60,
         hour = ((now.hours() % 12) / 12) * 360 + 90 + minute / 12;
 
-    $('#hour').css("transform", "rotate(" + hour + "deg)");
-    $('#minute').css("transform", "rotate(" + minute + "deg)");
-    $('#second').css("transform", "rotate(" + second + "deg)");
+    jQuery_3_4_1('#hour').css("transform", "rotate(" + hour + "deg)");
+    jQuery_3_4_1('#minute').css("transform", "rotate(" + minute + "deg)");
+    jQuery_3_4_1('#second').css("transform", "rotate(" + second + "deg)");
 }, 1000);
--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-var el = $('svg path.el'), index = 0;
+var el = jQuery_3_4_1('svg path.el'), index = 0;
 el.each(function() {
-    $(el[index]).attr('stroke-dasharray', el[index++].getTotalLength());
+    jQuery_3_4_1(el[index]).attr('stroke-dasharray', el[index++].getTotalLength());
 });
